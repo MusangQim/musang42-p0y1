a32fbfe2bc99d4e0a31297e0e1cb68f99bf9f360
MusangQim <musangqim03@gmail.com>
2026-03-16 15:00:56 +0800

    add idiom

--- ex0/ft_garden_intro.py
+++ ex0/ft_garden_intro.py
@@ -1,4 +1,8 @@
-def __name__(name, height, age):
-    print("=== Welcome to My Garden ===")
-    print("=== End of Program ===")
-
+if __name__ == '__main__':
+    
+    name = "Rose"
+    height = 25
+    age = 30
+    print("Plant: ", name)
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
