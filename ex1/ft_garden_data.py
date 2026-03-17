ee01a2f3d550df0d61e7cd14c737916d37cc2f8e
MusangQim <musangqim03@gmail.com>
2026-03-18 00:58:37 +0800

    add three type of plant

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -10,6 +10,8 @@ def main():
     p2 = Plant("Sunflower", 80, 45)
     p3 = Plant("Cactus", 15, 120)
     print(p1.name, ":", p1.height, "cm", p1.age, "days old")
+    print(p2.name, ":", p2.height, "cm", p2.age, "days old")
+    print(p3.name, ":", p3.height, "cm", p3.age, "days old")
 
 if __name__ == "__main__":
     main()
