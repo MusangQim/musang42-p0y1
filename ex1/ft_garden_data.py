66d7d136b14388c0672353350c079d8be47d126e
MusangQim <musangqim03@gmail.com>
2026-03-18 01:16:43 +0800

    fix little error

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -5,13 +5,13 @@ class Plant:
         self.age = age
 
 def main():
-    print("=== Garden Plant Registry===")
+    print("=== Garden Plant Registry ===")
     p1 = Plant("Rose", 25, 30)
     p2 = Plant("Sunflower", 80, 45)
     p3 = Plant("Cactus", 15, 120)
-    print(p1.name, ":", p1.height, "cm,", p1.age, "days old")
-    print(p2.name, ":", p2.height, "cm,", p2.age, "days old")
-    print(p3.name, ":", p3.height, "cm,", p3.age, "days old")
+    print(f"{p1.name}: {p1.height}cm, {p1.age} days old")
+    print(f"{p2.name}: {p2.height}cm, {p2.age} days old")
+    print(f"{p3.name}: {p3.height}cm, {p3.age} days old")
 
 if __name__ == "__main__":
     main()
