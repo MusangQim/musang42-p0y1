782ba2711d94591ae9515be3bc66233d6d90dc79
MusangQim <musangqim03@gmail.com>
2026-03-19 00:29:06 +0800

    change jap

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -1,12 +1,17 @@
 class   Plant:
-    def __init__(self, name: str, ) -> None:
+    def __init__(self, name: str, height: int, age: int ) -> None:
         self.name = name
+        self.height = height
+        self.age = age
 
 
-def grow(self) -> int:
+    def grow(self) -> None:
 
-def age(self) -> int:
 
-def get_info(self) -> None:
+    def age(self) -> int:
+
+
+    def get_info(self) -> None:
+
 
 def main() -> None:
