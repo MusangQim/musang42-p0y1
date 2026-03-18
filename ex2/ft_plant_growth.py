7b8ce085196ef54e08761af1902fed0d20d30d17
MusangQim <musangqim03@gmail.com>
2026-03-19 00:13:59 +0800

    put f(x) then think how the process

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -1,2 +1,12 @@
 class   Plant:
-    def __init__(self, name: str, 
+    def __init__(self, name: str, ) -> None:
+        self.name = name
+
+
+def grow(self) -> int:
+
+def age(self) -> int:
+
+def get_info(self) -> None:
+
+def main() -> None:
