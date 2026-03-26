b5e582786ea5af8abb6659a9b5f28d92ec6b3fb2
MusangQim <musangqim03@gmail.com>
2026-03-27 02:53:29 +0800

    00 ex

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,8 @@
+def __init__(self, name, height, age):
+    self.name = name
+    self.height = height
+    self.age = age
+    print('=== Welcome to My Garden ===')
+    print('=== End of Program ===')
+
+if __name__ == "__main__"
