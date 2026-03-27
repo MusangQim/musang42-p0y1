0df46c23b7199b1eab984a1f09759ec591d347ed
MusangQim <musangqim03@gmail.com>
2026-03-27 21:43:17 +0800

    ex4 added

---
+++ ex4/ft_garden_security.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class Plant:
+    def __init__(self, name, height, age) -> None:
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def show()
+
+    def set_height(self) -> None:
+
+    def get_age(self) -> None:
+
+
+if __name__ = "__main__":
+    print("=== Garden Security System ===")
