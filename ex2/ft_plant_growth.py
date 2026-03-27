9c1782146dddd205d3e2562dee4a2dc1026b24b2
MusangQim <musangqim03@gmail.com>
2026-03-27 18:45:36 +0800

    ex02 done

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python3
 
 class Plant:
-    def __init__(self, name, height, age) -> None:
+    def __init__(self, name: str, height: float, days: int) -> None:
         self.name = name
+        self.start = height
         self.height = height
-        self.age = age
-
+        self.days = days
 
     def show(self) -> None:
-        print(self.name)
+        print(f"{self.name}: {self.height}cm, {self.days} days old")
 
 
     def grow(self) -> None:
-        self.height = height + 0.8
+        self.height = round(self.height + 0.8, 2)
 
 
     def age(self) -> None:
-        self.age = age + 1
+        self.days = self.days + 1
 
 
 if __name__ == "__main__":
     print("=== Garden Plant Growth ===")
     rose = Plant("Rose", 25.0, 30)
-    for days in range(7):
-        print(f"=== Day: {days} ===")
-        print(f"{rose.show}: {rose.grow}cm, {rose.age} days old")
-    print("Growth this week: 6cm")
+    for x in range(1, 8):
+        print(f"=== Day {x} ===")
+        rose.show()
+        rose.grow()
+        rose.age()
+    growth = round(rose.height - rose.start)
+    print(f"Growth this week: {growth}cm")
