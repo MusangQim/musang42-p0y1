41f46369919f4b48d9701c55350b48db6ca93b2c
MusangQim <musangqim03@gmail.com>
2026-03-29 19:12:21 +0800

    add classes and methods

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -3,16 +3,24 @@
 class Plant:
     def __init__(self, name: str, height: float, age: int) -> None:
         self.name = name
-        self.height = height
-        self.age = age
+        self._height = height
+        self._age = age
 
 class Flower(Plant):
     def __init__(self, name: str, height: float, age: int, color: str) -> None:
         super().__init__(name, height, age)
         self._color = color
-        self.bloom = bloom
+    
+    def bloom(self) -> None:
+
 
 class Tree(Plant):
-    def __init__(self, name: str, height: float, age: int) -> None:
+    def __init__(self, name: str, height: float, age: int, trunk_diameter: int) -> None:
         super().__init__(name, height, age)
-        self.
+        self._trunk_diameter = trunk_diameter
+
+    def produce_shade(self) -> None:
+
+
+if __name__ == "__main__":
+    print("=== Garden Plant Types ===")
