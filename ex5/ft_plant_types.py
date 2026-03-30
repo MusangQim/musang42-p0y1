07b56c1445f8d7924f8d2505ca2ff121441fe4a4
MusangQim <musangqim03@gmail.com>
2026-03-30 22:01:53 +0800

    added new class and attributes

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -6,12 +6,25 @@ class Plant:
         self._height = height
         self._age = age
 
+    def show(self) -> None:
+        print(f"{self.name}: {self._height}cm, {self._age} days old")
+
 class Flower(Plant):
     def __init__(self, name: str, height: float, age: int, color: str) -> None:
         super().__init__(name, height, age)
         self._color = color
+        self._bloomed = False
     
     def bloom(self) -> None:
+        self._bloomed = True
+
+    def show(self) -> None:
+        super().show()
+        print(f"Color: {self._color}")
+        if self._bloomed == False:
+            print(f"{self.name} has not bloomed yet")
+        else:
+            print(f"{self.name} is blooming beautifully!")
 
 
 class Tree(Plant):
@@ -20,7 +33,24 @@ class Tree(Plant):
         self._trunk_diameter = trunk_diameter
 
     def produce_shade(self) -> None:
+       
+    def show(self) -> None:
+        super().show()
+        print(f"Trunk diameter: {self._trunk_diameter}")
+        print(f"Tree {self.name} now produces a shade of {self._height}cm long and {self.trunk_diameter}cm wide.")
 
+
+class Vegetable(Plant):
+    def __init__(self, name: str, height: float, age: int, harvest_season: str, nutritional_value: int) -> None:
+        super().__init__(name, height, age)
+        self.harvest_season: season
+        self.nutritional_value: value
+
+    def show(self) -> None:
+        super().show()
+        print(f"Harvest season: {self.harvest_season}")
+        print(f"Nutritional value: {self.nutritional_value}")
 
 if __name__ == "__main__":
     print("=== Garden Plant Types ===")
+    
